cda62e364bdda36fa577062d5d19dc9791df2b19
Andrew Yegorov <andrewyegorov@gmail.com>
2021-03-28 13:16:09 +0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
 	
 
     <!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
